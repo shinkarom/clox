aca96d2b8770f90767c7f09b56808fcd227cae40
shinkarom <shinkarom@gmail.com>
2023-03-01 12:51:59 +0200

    Initial commit; part of chapter 1

---
+++ src/chunk.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "common.h"
+
+typedef enum {
+	OP_RETURN,
+} OpCode;
+
+typedef struct {
+	int count;
+	int capacity;
+	uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
