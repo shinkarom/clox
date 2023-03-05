d1501c39a7fa0c4362e7359969134b38029e89ff
shinkarom <shinkarom@gmail.com>
2023-03-05 10:48:36 +0200

    Add compiler.c and scanner.c

--- src/vm.h
+++ src/vm.h
@@ -15,11 +15,11 @@ typedef struct {
 typedef enum {
 	INTERPRET_OK,
 	INTERPRET_COMPILE_ERROR,
-	INTERPRETRUNTIME_ERROR
+	INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
