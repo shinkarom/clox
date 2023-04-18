f3f5c579f68ad26fc2225d57b2e2f74be6fe0bbe
shinkarom <shinkarom@gmail.com>
2023-04-18 11:16:36 +0300

    Add variable assignment

--- src/compiler.c
+++ src/compiler.c
@@ -32,7 +32,7 @@ typedef enum {
 	PREC_PRIMARY
 } Precedence;
 
-typedef void (*ParseFn)();
+typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
 	ParseFn prefix;
@@ -228,7 +228,7 @@ static void statement() {
 	}
 }
 
-static void binary() {
+static void binary(bool canAssign) {
 	TokenType operatorType = parser.previous.type;
 	ParseRule* rule = getRule(operatorType);
 	parsePrecedence((Precedence)(rule->precedence + 1));
@@ -248,7 +248,7 @@ static void binary() {
 	}
 }
 
-static void literal() {
+static void literal(bool canAssign) {
 	switch(parser.previous.type) {
 		case TOKEN_FALSE: emitByte(OP_FALSE); break;
 		case TOKEN_NIL: emitByte(OP_NIL);; break;
@@ -257,22 +257,37 @@ static void literal() {
 	}
 }
 
-static void grouping() {
+static void grouping(bool canAssign) {
 	expression();
 	consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
 
-static void number() {
+static void number(bool canAssign) {
 	double value = strtod(parser.previous.start, NULL);
 	emitConstant(NUMBER_VAL(value));
 }
 
-static void string() {
+static void string(bool canAssign) {
 	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
 		parser.previous.length - 2)));
 }
 
-static void unary() {
+static void namedVariable(Token name, bool canAssign) {
+	uint8_t arg = identifierConstant(&name);
+	
+	if(canAssign && match(TOKEN_EQUAL)) {
+		expression();
+		emitBytes(OP_SET_GLOBAL, arg);
+	} else {
+		emitBytes(OP_GET_GLOBAL, arg);
+	}
+}
+
+static void variable(bool canAssign) {
+	namedVariable(parser.previous, canAssign);
+}
+
+static void unary(bool canAssign) {
 	TokenType operatorType = parser.previous.type;
 
 	parsePrecedence(PREC_UNARY);
@@ -304,7 +319,7 @@ ParseRule rules[] = {
 	[TOKEN_GREATER_EQUAL] = {NULL,binary,PREC_COMPARISON},
 	[TOKEN_LESS] = {NULL,binary,PREC_COMPARISON},
 	[TOKEN_LESS_EQUAL] = {NULL,binary,PREC_COMPARISON},
-	[TOKEN_IDENTIFIER] = {NULL,NULL,PREC_NONE},
+	[TOKEN_IDENTIFIER] = {variable,NULL,PREC_NONE},
 	[TOKEN_STRING] = {string,NULL,PREC_NONE},
 	[TOKEN_NUMBER] = {number,NULL,PREC_NONE},
 	[TOKEN_AND] = {NULL,NULL,PREC_NONE},
@@ -335,12 +350,17 @@ static void parsePrecedence(Precedence precedence) {
 		return;
 	}
 
-	prefixRule();
+	bool canAssign = precedence <= PREC_ASSIGNMENT;
+	prefixRule(canAssign);
 
 	while (precedence <= getRule(parser.current.type)->precedence) {
 		advance();
 		ParseFn infixRule = getRule(parser.previous.type)->infix;
-		infixRule();
+		infixRule(canAssign);
+	}
+	
+	if(canAssign && match(TOKEN_EQUAL)) {
+		error("Invalid assignment target.");
 	}
 }
 
