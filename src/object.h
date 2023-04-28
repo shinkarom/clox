b200a6a5f9b3faf9ae01112faee8a91c3a70c7e2
shinkarom <shinkarom@gmail.com>
2023-04-28 10:54:23 +0300

    Add function objects

--- src/object.h
+++ src/object.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+	OBJ_FUNCTION,
 	OBJ_STRING,
 } ObjType;
 
@@ -25,6 +30,14 @@ struct ObjString {
 	uint32_t hash;
 };
 
+typedef struct {
+	Obj obj;
+	int arity;
+	Chunk chunk;
+	ObjString* name;
+} ObjFunction;
+
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
